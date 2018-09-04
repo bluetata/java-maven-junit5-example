ef735ed752628864aca4f76466931d44284ba20c
bluetata <dietime1943@gmail.com>
2018-09-04 15:08:48 +0800

    update basic test class

--- src/test/java/com/example/junit5/test/basic/HelloWorldTest.java
+++ src/test/java/com/example/junit5/test/basic/HelloWorldTest.java
@@ -2,6 +2,7 @@ package com.example.junit5.test.basic;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,16 +17,19 @@ class HelloWorldTest {
 
     private HelloWorld helloWorld;
     @BeforeEach
+    @DisplayName("Before run test case to init resource")
     void init() {
         helloWorld = new HelloWorld();
     }
 
     @Test
+    @DisplayName("Run test case.")
     void sayHelloWorld() {
         assertEquals("Hello World", helloWorld.sayHelloWorld());
     }
 
     @AfterEach
+    @DisplayName("Finish Test to destroy resource")
     void tearDown() {
         helloWorld = null;
     }
